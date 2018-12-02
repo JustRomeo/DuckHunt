c3dd031dbe32f869a55e707f0fcbcd02e4409b64
Roméo <romeo@localhost.localdomain>
2018-12-02 07:27:17 -0500

    minor modification

--- main.c
+++ main.c
@@ -32,9 +32,8 @@ int main(int ac, char **argv)
 
     if (ac > 1 && ac < 3 && argv[1][0] == '-' && argv[1][1] == 'h')
         usage();
-    else if (ac == 1) {
+    else if (ac == 1)
         Gamemode(&Game);
-    }
     else
         write(1, "Unknow arguments :(\n", 20);
     return (0);
